67b6ff48cf57c486718971d9c0c7519bbc5dab4e
weisstep <stephanweiss90218@gmail.com>
2024-12-29 18:57:45 +0100

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Item } from "./item";
+import { ItemComponent } from "./item/item.component";
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule, ItemComponent],
+})
+export class AppComponent {
+  componentTitle = "My To Do List";
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ];
+
+  addItem(description: string) {
+    if (!description) return;
+  
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    );
+  }
+
+  remove(item: Item) {
+    this.allItems.splice(this.allItems.indexOf(item), 1);
+  }
+  
+}
